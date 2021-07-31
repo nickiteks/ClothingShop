0220fb83eb1904e5896a1c6560e7d25b4b583e07
nickiteks <55086354+nickiteks@users.noreply.github.com>
2021-07-31 17:21:19 +0400

    add templates

--- shop/mainapp/views.py
+++ shop/mainapp/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+def store(request):
+    context = {}
+    return render(request, 'mainapp/index.html', context)
+
+
+def cart(request):
+    context = {}
+    return render(request, 'mainapp/cart.html', context)
+
+
+def checkout(request):
+    context = {}
+    return render(request, 'mainapp/checkout.html', context)
