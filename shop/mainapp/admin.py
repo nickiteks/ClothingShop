f3376d0754e8c8b4e436c8e3533adfd4b1e6c519
nickiteks <55086354+nickiteks@users.noreply.github.com>
2021-08-16 22:02:00 +0400

    t 17'51

--- shop/mainapp/admin.py
+++ shop/mainapp/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import *
+#1111
+admin.site.register(Customer)
+admin.site.register(Product)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(ShippingAdder)
 
-# Register your models here.
+
+
+
+
