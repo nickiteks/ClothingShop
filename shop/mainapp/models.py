e4256a7dca65c890d856c9a82779e6b0710d8d32
nickiteks <55086354+nickiteks@users.noreply.github.com>
2021-08-25 01:53:35 +0400

    t 0'00

--- shop/mainapp/models.py
+++ shop/mainapp/models.py
@@ -37,6 +37,7 @@ class Order(models.Model):
     def __str__(self):
         return str(self.id)
 
+    @property
     def get_cart_total(self):
         orderitems = self.orderitem_set.all()
         total = sum([item.get_total for item in orderitems])
