d0d38aa20764125f8dda9fe8b73d16f84abcb937
nickiteks <55086354+nickiteks@users.noreply.github.com>
2021-12-13 02:24:28 +0400

    test

--- shop/mainapp/tests.py
+++ shop/mainapp/tests.py
@@ -12,10 +12,8 @@ from selenium.webdriver.support.select import Select
 
 class ShopTest(LiveServerTestCase):
 
-
     def test_auth(self):
         options = webdriver.ChromeOptions()
-        #options.add_argument('headless')
         options.add_argument('--kiosk')
         selenium = webdriver.Chrome('C:\\Users\\NULS\\PycharmProjects\\ClothingShop\\shop\\static\\chromedriver.exe',
                                     options=options)
@@ -23,15 +21,151 @@ class ShopTest(LiveServerTestCase):
 
         selenium.find_element_by_xpath('//a[contains(@href,"/login/")]').click()
 
-        input_username = selenium.find_element(by='name',value="username")
-        input_password = selenium.find_element(by='name',value="password")
+        input_username = selenium.find_element(by='name', value="username")
+        input_password = selenium.find_element(by='name', value="password")
 
         input_username.send_keys("admin")
         input_password.send_keys('1111')
 
         selenium.find_element(value="Login").click()
 
-        selenium.get("http://127.0.0.1:8000/admin/")
+    def test_registr(self):
+        options = webdriver.ChromeOptions()
+        options.add_argument('--kiosk')
+        selenium = webdriver.Chrome('C:\\Users\\NULS\\PycharmProjects\\ClothingShop\\shop\\static\\chromedriver.exe',
+                                    options=options)
+        selenium.get("http://127.0.0.1:8000/")
 
-        time.sleep(5)
+        selenium.find_element_by_xpath('//a[contains(@href,"/register/")]').click()
+
+        input_username = selenium.find_element(by='name', value="username")
+        input_email = selenium.find_element(by='name', value="email")
+        input_password1 = selenium.find_element(by='name', value="password1")
+        input_password2 = selenium.find_element(by='name', value="password2")
+
+        input_username.send_keys('test')
+        input_email.send_keys('test@gmail.com')
+        input_password1.send_keys('test12345678')
+        input_password2.send_keys('test12345678')
+
+        selenium.find_element(value="Register").click()
+
+    def test_choose_product(self):
+        options = webdriver.ChromeOptions()
+        options.add_argument('--kiosk')
+        selenium = webdriver.Chrome('C:\\Users\\NULS\\PycharmProjects\\ClothingShop\\shop\\static\\chromedriver.exe',
+                                    options=options)
+        selenium.get("http://127.0.0.1:8000/")
+        selenium.find_element_by_xpath('//a[contains(@href,"/2/")]').click()
+
+    def test_to_cart_with_index(self):
+        options = webdriver.ChromeOptions()
+        options.add_argument('--kiosk')
+        selenium = webdriver.Chrome('C:\\Users\\NULS\\PycharmProjects\\ClothingShop\\shop\\static\\chromedriver.exe',
+                                    options=options)
+        selenium.get("http://127.0.0.1:8000/")
+
+        selenium.find_element(by='id', value=2).click()
+
+    def test_to_cart_with_product_page(self):
+        options = webdriver.ChromeOptions()
+        options.add_argument('--kiosk')
+        selenium = webdriver.Chrome('C:\\Users\\NULS\\PycharmProjects\\ClothingShop\\shop\\static\\chromedriver.exe',
+                                    options=options)
+        selenium.get("http://127.0.0.1:8000/")
+
+        selenium.find_element_by_xpath('//a[contains(@href,"/2/")]').click()
+        selenium.find_element(by='id', value=2).click()
+
+    def test_comment(self):
+        options = webdriver.ChromeOptions()
+        options.add_argument('--kiosk')
+        selenium = webdriver.Chrome('C:\\Users\\NULS\\PycharmProjects\\ClothingShop\\shop\\static\\chromedriver.exe',
+                                    options=options)
+        selenium.get("http://127.0.0.1:8000/")
+
+        selenium.find_element_by_xpath('//a[contains(@href,"/login/")]').click()
+
+        input_username = selenium.find_element(by='name', value="username")
+        input_password = selenium.find_element(by='name', value="password")
+
+        input_username.send_keys("admin")
+        input_password.send_keys('1111')
+
+        selenium.find_element(value="Login").click()
+
+        selenium.get("http://127.0.0.1:8000/")
+
+        selenium.find_element_by_xpath('//a[contains(@href,"/2/")]').click()
+
+        input_comment = selenium.find_element(by="id", value="30")
+        input_comment.send_keys('test')
+
+        selenium.find_element(by="id", value="40").click()
+
+    def test_add_product(self):
+        options = webdriver.ChromeOptions()
+        options.add_argument('--kiosk')
+        selenium = webdriver.Chrome('C:\\Users\\NULS\\PycharmProjects\\ClothingShop\\shop\\static\\chromedriver.exe',
+                                    options=options)
+        selenium.get("http://127.0.0.1:8000/")
+
+        selenium.find_element_by_xpath('//a[contains(@href,"/login/")]').click()
+
+        input_username = selenium.find_element(by='name', value="username")
+        input_password = selenium.find_element(by='name', value="password")
+
+        input_username.send_keys("admin")
+        input_password.send_keys('1111')
+
+        selenium.find_element(value="Login").click()
+
+        selenium.get("http://127.0.0.1:8000/admin/mainapp/product/add/")
+
+        selenium.find_element(by='name', value="name").send_keys('test_product')
+        selenium.find_element(by='name', value="price").send_keys('1')
+        selenium.find_element(by='name', value="description").send_keys('test')
+        selenium.find_element(by='name', value="_save").click()
+
+    def test_change_product(self):
+        options = webdriver.ChromeOptions()
+        options.add_argument('--kiosk')
+        selenium = webdriver.Chrome('C:\\Users\\NULS\\PycharmProjects\\ClothingShop\\shop\\static\\chromedriver.exe',
+                                    options=options)
+        selenium.get("http://127.0.0.1:8000/")
+
+        selenium.find_element_by_xpath('//a[contains(@href,"/login/")]').click()
+
+        input_username = selenium.find_element(by='name', value="username")
+        input_password = selenium.find_element(by='name', value="password")
+
+        input_username.send_keys("admin")
+        input_password.send_keys('1111')
+
+        selenium.find_element(value="Login").click()
+
+        selenium.get("http://127.0.0.1:8000/admin/mainapp/product/12/change/")
+
+        selenium.find_element(by='name', value="name").send_keys('test_product_changed')
+        selenium.find_element(by='name', value="_save").click()
+
+    def test_delete_product(self):
+        options = webdriver.ChromeOptions()
+        options.add_argument('--kiosk')
+        selenium = webdriver.Chrome('C:\\Users\\NULS\\PycharmProjects\\ClothingShop\\shop\\static\\chromedriver.exe',
+                                    options=options)
+        selenium.get("http://127.0.0.1:8000/")
+
+        selenium.find_element_by_xpath('//a[contains(@href,"/login/")]').click()
+
+        input_username = selenium.find_element(by='name', value="username")
+        input_password = selenium.find_element(by='name', value="password")
+
+        input_username.send_keys("admin")
+        input_password.send_keys('1111')
+
+        selenium.find_element(value="Login").click()
+
+        selenium.get("http://127.0.0.1:8000/admin/mainapp/product/12/delete/")
+        selenium.find_element(by='xpath', value="//input[@type='submit']").click()
 
